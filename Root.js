2da72cf5a1d1055f2220d17bed7b03f1b1fddbc3
Seunghyeong Lee <seunghyeong27@gmail.com>
2021-03-24 08:30:56 +0900

    Feature :: 프로젝트 생성

---
+++ Root.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Root = () => {
+    return (
+        <View>
+            <Text>hi asdfasdfsdafasdfsadfasdf</Text>
+        </View>
+    )
+}
+
+export default Root
+
+const styles = StyleSheet.create({})
