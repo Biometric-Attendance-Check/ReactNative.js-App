9257d76bf4224e859455ae6a264170081a994dce
Seunghyeong Lee <seunghyeong27@gmail.com>
2021-03-30 20:36:50 +0900

    Feature :: 타이틀, 틀

--- Root.js
+++ Root.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Text, View } from 'react-native'
 import Main from './src/Screen/Main'
 
 const Root = () => {
--- src/Screen/Main.js
+++ src/Screen/Main.js
@@ -1,27 +1,47 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Dimensions } from 'react-native'
 import { getStatusBarHeight } from "react-native-status-bar-height"; 
 
+const { width, height } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
-    flexBlue: {
+    flexText: {
         display: 'flex',
-        flex: 1,
-        backgroundColor: 'steelblue',
+        marginTop: getStatusBarHeight()+height*0.01,
+        marginLeft: width*0.05,
+        fontSize: 24,
+        fontWeight: 'bold',
     },
     flexRed: {
         display: 'flex',
-        flex: 1,
-        marginTop: getStatusBarHeight(),
-        backgroundColor: 'red',
+        flex: 7,
+        margin: width*0.05,
+        marginBottom: 0,
+        borderRadius: 15,
+        backgroundColor: 'rgb(225,224,255)',
     },
+    flexBlue: {
+        display: 'flex',
+        flex: 7,
+        margin: width*0.05,
+        borderRadius: 15,
+        backgroundColor: 'rgb(188,224,255)',
+    },
+    flexWhite: {
+        display: 'flex',
+        backgroundColor: 'white',
+        flex: 1,
+    }
 });
 
 const Main = () => {
+    console.log(width, height)
     return (
-        <>
-        <View style={styles.flexRed}></View>
-        <View style={styles.flexBlue}></View>
-        </>
+        <View style={styles.flexWhite}>
+            <Text style={styles.flexText}>3WDJ 지문인식</Text>
+            <View style={styles.flexRed}></View>
+            <View style={styles.flexBlue}></View>
+        </View>
     )
 }
 
