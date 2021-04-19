31001379701b1c32a4b4b209b4a0f7e1a865a5e4
Seunghyeong Lee <seunghyeong27@gmail.com>
2021-04-19 17:39:40 +0900

    Feature :: 와이파이 설정

--- src/Components/Biometric.js
+++ src/Components/Biometric.js
@@ -54,15 +54,15 @@ const Biometric = (props) => {
         await axios.post(`http://13.209.70.126/app/fingerPrint_test.php`, {
             "userDevice":props.uid,
         }).then((res) => {
-            if(res.data.access == false){
-                Alert.alert("삐빅.. 와이파이가 다릅니다..\nYJU-BON***_5G에 연결해주세요.")
-            } else{
+            // if(res.data.access == false){
+            //     // Alert.alert("삐빅.. 와이파이가 다릅니다..\nYJU-BON***_5G에 연결해주세요.")
+            // } else{
                 setUserData(res.data)
 
                 statusText === '등교'
                 ? Alert.alert('등교했습니다.')
                 : Alert.alert('집에 갑시다!')
-            }
+            // }
         })
     }
 
@@ -70,9 +70,19 @@ const Biometric = (props) => {
         Alert.alert('이미 귀가 하셨습니다.')
     }
 
+    const ipCheck = async () => {
+        await axios.post(`http://13.209.70.126/app/ip_check_app.php`).then(res => {
+            if(res.data.access == true){
+                prompt()
+            } else{
+                Alert.alert("삐빅.. 와이파이가 다릅니다..\nYJU-BON***_5G에 연결해주세요.")
+            }
+        })
+    }
+
     return (
         <View style={styles.flexGrey}>
-            <TouchableOpacity style={styles.bioTouch} onPress={(statusText != 'x') ? prompt : stop}>
+            <TouchableOpacity style={styles.bioTouch} onPress={(statusText != 'x') ? ipCheck : stop}>
                 <Text style={styles.bioText}>{props.text?props.text:statusText}</Text>
             </TouchableOpacity>
         </View>
